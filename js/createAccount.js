4a5ce394936452ce87607a861daa9a81086eedf3
Jana Shyti <jshyti@eagles.bridgewater.edu>
2024-02-14 11:23:18 -0500

    webapptest3

---
+++ js/createAccount.js
@@ -0,0 +1,32 @@
+async function fetchUsingDataFromForm(){
+    const mssg8 = document.querySelector("#message8")
+    const input = document.querySelector("#input8")
+    const url = "https://janas-api-server.azurewebsites.net/user"
+    const data = {
+        code: InputDeviceInfo.value
+    }
+
+
+const options = {
+    method: "POST",
+    headers: {"Content-Type": "application/json" },
+    body: JSON.stringify(data)
+
+} 
+
+let response = await fetch(url, options)
+
+if (response.status == 200){
+    const obj = await response.json()
+    mssg8.innerHTML = obj.message
+}
+else if (response.status == 401){
+    const obj = await response.json()
+    mssg8.innerHTML = "ERROR: " + obj.message
+}
+else if (response.status == 400){
+    mssg8.innerHTML = "Server error"
+}
+}
+
+document.querySelector("#button8").addEventListener("click", fetchUsingDataFromForm)
